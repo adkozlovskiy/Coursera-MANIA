6927106c751040a6bac36ff3ae33b8065c5da9ce
Alexey Kozlovskiy <whiititi@gmail.com>
2021-02-02 17:51:09 +0300

    Add files via upload

---
+++ Practice Four. Lists, RecyclerView and Adapter/app/src/main/java/com/example/practicethree/objects/Text.java
@@ -0,0 +1,16 @@
+package com.example.practicethree.objects;
+
+
+public class Text extends RecyclerObject {
+
+    private String text;
+
+    public Text(String text) {
+        super();
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
